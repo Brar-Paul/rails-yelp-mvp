5a711b7fb26382627e999a8e4eb24d2c1f453c87
Paul Brar <ibrar@ualberta.ca>
2020-11-11 12:23:56 +0100

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+categories = %w[chinese italian japanese french belgian]
+puts "Importing restaurants"
+10.times do
+  resto = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: categories.sample
+  )
+  resto.save!
+end
+puts "Restaurants have been imported"
+puts "Importing Reviews"
+100.times do
+  review = Review.new(
+    rating: rand(0..5),
+    content: Faker::Restaurant.review,
+    restaurant_id: rand(1..10)
+  )
+  review.save!
+end
+puts "Reviews have been imported, good to go!"
